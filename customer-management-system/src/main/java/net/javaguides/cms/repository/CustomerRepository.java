71c23ebd91ee66c7411cbba807c0415e155503bb
Salma Glorify <125642901+salmaglorify@users.noreply.github.com>
2023-04-11 13:07:51 +0700

    Delete customer-management-system directory

--- customer-management-system/src/main/java/net/javaguides/cms/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package net.javaguides.cms.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import net.javaguides.cms.entity.Customer;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-
-}
